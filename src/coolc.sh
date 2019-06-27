b962fa706b80f04aef63b2f14416c307dddf088c
Luis E Martínez <l.padron@estudiantes.matcom.uh.cu>
2019-06-27 12:28:05 -0400

    Update

--- src/coolc.sh
+++ src/coolc.sh
@@ -6,6 +6,8 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 echo "PyCool537 v2.1"
 echo "Copyright (c) 2019: Luis E Martinez, Marcos Alderete, Roberto Balboa"
 
+./compile.sh INPUT_FILE -o OUTPUT_FILE
+
 # Llamar al compilador
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
 
